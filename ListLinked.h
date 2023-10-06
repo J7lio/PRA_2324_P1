8640d5ee3605836a665385a17cc2c6f42a29a90e
J7lio <juliodela.jm@gmail.com>
2023-10-07 00:48:42 +0200

    Añadidos testListLinked.cpp y modificado makefile

--- ListLinked.h
+++ ListLinked.h
@@ -8,24 +8,29 @@ class ListLinked : public List<T> {
 		Node<T>* first;
 		int n;
 	public:
+		//Funcion para ayudar en otras partes del codigo.
+		Node<T>* getNode(int pos){
+			Node<T>* aux = first;
+			for(int i = 0; i < pos; i++){
+				aux = aux->next;
+			}
+			return aux;
+		}
+
 		void insert(int pos, T e){
 			if(pos < 0 || pos > n) throw std::out_of_range("Posicion fuera de rango");
 
 			if(n == 0){
-				first = new Node<T>*;
-				first->data = e;
+				first = new Node<T>(e);
 				first->next = nullptr;
 			}else if(pos == 0){
-				Node<T>* aux = new Node<T>*;
+				Node<T>* aux = new Node<T>(e);
 				aux->next = first;
-				aux->data = e;
 				first = aux;
 			}else{
-				Node<T>* aux = new Node<T>*;
-				aux->next = this[pos];
-				aux->data = e;
-				this[pos-1]->next = aux;
-			}
+				Node<T>* aux = new Node<T>(e);
+				aux->next = getNode(pos);
+				getNode(pos-1)->next = aux;			}
 			n++;
 		}
 
@@ -40,17 +45,30 @@ class ListLinked : public List<T> {
 		T remove(int pos){
 			if(pos < 0 || pos >= n) throw std::out_of_range("Posicion fuera de rango");
 
-			this[pos-1]->next = this[pos]->next;
-			T datoEliminado = this[pos]->data;
-			delete this[pos];
+			Node<T>* NodoaBorrar = getNode(pos);
+			if(pos==0){
+                first = NodoaBorrar->next;
+			}else{
+			    Node<T>* prev = getNode(pos-1);
+			    prev->next = NodoaBorrar->next;
+			}
 
-			return datoEliminado;
+			T d = NodoaBorrar->data;
+			delete NodoaBorrar;
+
+			n--;
+
+			return d;
 		}
 
 		T get(int pos){
 			if(pos < 0 || pos >= n) throw std::out_of_range("Posicion fuera de rango");
 
-			return this[pos]->data;
+			Node<T>* aux = first;
+			for(int i = 0; i < pos; i++){
+				aux = aux->next;
+			}
+			return aux->data;
 		}
 
 		int search(T e){
@@ -58,6 +76,7 @@ class ListLinked : public List<T> {
 			int i = 0;
 			while(aux != nullptr){
 				if(aux->data == e) return i;
+				aux = aux->next;
 			}
 			return -1;
 		}
@@ -72,6 +91,7 @@ class ListLinked : public List<T> {
 
 		ListLinked(){
 			first = nullptr;
+			n = 0;
 		}
 
 		~ListLinked(){
@@ -83,13 +103,7 @@ class ListLinked : public List<T> {
 		}
 
 		T operator[](int pos){
-			if(pos >= n) throw("Posicion fuera de rango");
-
-			Node<T>* aux = first;
-			for(int i = 0; i < pos; i++){
-				aux = aux->next;
-			}
-			return aux;
+			return get(pos);
 		}
 
 		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
@@ -97,7 +111,7 @@ class ListLinked : public List<T> {
 
 			Node<T>* aux = list.first;
 			for(int i = 0; i < list.n; i++){
-				out << "\n" << aux.data;
+				out << "\n" << aux->data;
 				aux = aux->next;
 			}
 
