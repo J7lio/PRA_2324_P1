2748cb4c9ad8c2a8bf14fc2a1c8fd6534ea7045f
J7lio <juliodela.jm@gmail.com>
2023-10-16 11:24:08 +0200

    Arreglado bugs clase Point2D funcionando

--- Point2D.cpp
+++ Point2D.cpp
@@ -5,19 +5,19 @@ Point2D::Point2D(double x=0, double y=0){
     this->y = y;
 }
 
-static double Point2D::distance(const Point2D &a, const Point2D &b){
+double Point2D::distance(const Point2D &a, const Point2D &b){
     return sqrt(pow(a.x-b.x, 2) + pow(a.y-b.y, 2));
 }
 
-bool Point2D::operator==(const Point2D &a, const Point2D &b){
+bool operator==(const Point2D &a, const Point2D &b){
     return a.x==b.x && a.y==b.y;
 }
 
-bool Point2D::operator!=(const Point2D &a, const Point2D &b){
+bool operator!=(const Point2D &a, const Point2D &b){
     return a.x!=b.x && a.y!=b.y;
 }
 
-std::ostream& Point2D::operator<<(std::ostream &out, const Point2D &p){
+std::ostream& operator<<(std::ostream &out, const Point2D &p){
     out << "(" << p.x << ", " << p.y << ")";
     return out;
 }
